5e5403329250f04be311b66e7ece82a7c12500a0
nkiyo <nakahara.kiyoshi@gmail.com>
2025-03-03 23:17:37 +0900

    Initial commit

---
+++ test/wsapi-example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WsapiExample from '../lib/wsapi-example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wsapi-example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WsapiExample.WsapiExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
